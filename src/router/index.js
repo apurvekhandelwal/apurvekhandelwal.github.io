0839a9ac3e529e6bb668fcbb0355ac2c5277d3ae
apurve khandelwal <apurvkhandelwal3355@gmail.com>
2026-02-26 17:02:14 +0530

    changes

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 import Home from "../views/Home.vue";
 
 const router = createRouter({
-    history: createWebHistory('/apurve-portfolio/'),
+    history: createWebHistory(),
     routes: [
         {
             path: '/',
